8eb32a63909ddd141de41e5e9adfe0658c679be4
Liliam Bronze <liliambronze@gmail.com>
2025-11-20 16:22:40 +0000

    Add special spike object interactions

--- General/Sprites/Level Misc/Map - Spikes.asm
+++ General/Sprites/Level Misc/Map - Spikes.asm
@@ -7,6 +7,12 @@ Map_Spikes_:
 		dc.w word_244F4-Map_Spikes_
 		dc.w word_2450E-Map_Spikes_
 		dc.w word_24534-Map_Spikes_
+		dc.w Map_Spikes_08-Map_Spikes_
+		dc.w Map_Spikes_09-Map_Spikes_
+		dc.w Map_Spikes_0A-Map_Spikes_
+		dc.w Map_Spikes_0B-Map_Spikes_
+		dc.w Map_Spikes_0C-Map_Spikes_
+		dc.w Map_Spikes_0D-Map_Spikes_
 word_24466:	dc.w 2
 		dc.b  $F0,   7,   0,   0, $FF, $F0
 		dc.b  $F0,   7,   0,   0,   0,   0
@@ -55,3 +61,27 @@ word_24534:	dc.w 8
 		dc.b  $10,  $D,   8,   0, $FF, $F0
 		dc.b  $20,  $D,   8,   0, $FF, $F0
 		dc.b  $30,  $D,   8,   0, $FF, $F0
+Map_Spikes_08:	dc.w 1
+		dc.b  $F0,   7,   0,   0, $FF, $F8
+Map_Spikes_09:	dc.w 3
+		dc.b  $F0,   7,   0,   0, $FF, $E8
+		dc.b  $F0,   7,   0,   0, $FF, $F8
+		dc.b  $F0,   7,   0,   0,   0,   8
+Map_Spikes_0A:	dc.w 5
+		dc.b  $F0,   7,   0,   0, $FF, $D8
+		dc.b  $F0,   7,   0,   0, $FF, $E8
+		dc.b  $F0,   7,   0,   0, $FF, $F8
+		dc.b  $F0,   7,   0,   0,   0,   8
+		dc.b  $F0,   7,   0,   0,   0, $18
+Map_Spikes_0B:	dc.w 7
+		dc.b  $F0,   7,   0,   0, $FF, $C8
+		dc.b  $F0,   7,   0,   0, $FF, $D8
+		dc.b  $F0,   7,   0,   0, $FF, $E8
+		dc.b  $F0,   7,   0,   0, $FF, $F8
+		dc.b  $F0,   7,   0,   0,   0,   8
+		dc.b  $F0,   7,   0,   0,   0, $18
+		dc.b  $F0,   7,   0,   0,   0, $28
+Map_Spikes_0C:	dc.w 1
+		dc.b  $F0,   3,   0,   0, $FF, $F8
+Map_Spikes_0D:	dc.w 1
+		dc.b  $F0,   3,   0,   4,   0,   0
