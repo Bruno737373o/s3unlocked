39f1155d132eceabd9ee3587f0338ddfa0edb159
Liliam Bronze <liliambronze@gmail.com>
2026-01-08 22:30:17 +0000

    Add missing continue icons

--- General/Continue/Map - Player Icons.asm
+++ General/Continue/Map - Player Icons.asm
@@ -5,14 +5,14 @@ Map_ContinueIcons_:
 		dc.w word_5CD36-Map_ContinueIcons_
 		dc.w word_5CD56-Map_ContinueIcons_
 		dc.w word_5CD5E-Map_ContinueIcons_
-		dc.w Map_ContinueIconAmy1-Map_ContinueIcons_
-		dc.w Map_ContinueIconAmy2-Map_ContinueIcons_
-		dc.w word_5CD12-Map_ContinueIcons_
-		dc.w word_5CD20-Map_ContinueIcons_
-		dc.w word_5CD12-Map_ContinueIcons_
-		dc.w word_5CD20-Map_ContinueIcons_
-		dc.w word_5CD12-Map_ContinueIcons_
-		dc.w word_5CD20-Map_ContinueIcons_
+		dc.w Map_ContinueIcon_Amy1-Map_ContinueIcons_
+		dc.w Map_ContinueIcon_Amy2-Map_ContinueIcons_
+		dc.w Map_ContinueIcon_Mighty1-Map_ContinueIcons_
+		dc.w Map_ContinueIcon_Mighty2-Map_ContinueIcons_
+		dc.w Map_ContinueIcon_Ray1-Map_ContinueIcons_
+		dc.w Map_ContinueIcon_Ray2-Map_ContinueIcons_
+		dc.w Map_ContinueIcon_MetalSonic1-Map_ContinueIcons_
+		dc.w Map_ContinueIcon_MetalSonic2-Map_ContinueIcons_
 		dc.w word_5CD3E-Map_ContinueIcons_
 		dc.w word_5CD46-Map_ContinueIcons_
 		dc.w word_5CD4E-Map_ContinueIcons_
@@ -27,18 +27,42 @@ word_5CD2E:	dc.w 1
 word_5CD36:	dc.w 1
 		dc.b  $F4,   6,   0, $10, $FF, $F8
 word_5CD3E:	dc.w 1
-		dc.b  $FC,   5,   0, $16, $FF, $F2
+		dc.b  $FC,   5,   0, $4C, $FF, $F2
 word_5CD46:	dc.w 1
-		dc.b  $FC,   5,   0, $1A, $FF, $F2
+		dc.b  $FC,   5,   0, $50, $FF, $F2
 word_5CD4E:	dc.w 1
-		dc.b  $FC,   5,   0, $1E, $FF, $F2
+		dc.b  $FC,   5,   0, $54, $FF, $F2
 word_5CD56:	dc.w 1
-		dc.b  $F4,   6, $40, $22, $FF, $F8
+		dc.b  $F4,   6, $40, $16, $FF, $F8
 word_5CD5E:	dc.w 1
-		dc.b  $F4,   6, $40, $28, $FF, $F8
-Map_ContinueIconAmy1:
+		dc.b  $F4,   6, $40, $1C, $FF, $F8
+Map_ContinueIcon_Amy1:
 		dc.w 1
-		dc.b  $F4,   6,   0, $2E, $FF, $F8
-Map_ContinueIconAmy2:
+		dc.b  $F4,   6,   0, $22, $FF, $F8
+Map_ContinueIcon_Amy2:
 		dc.w 1
-		dc.b  $F4,   6,   0, $34, $FF, $F8
+		dc.b  $F4,   6,   0, $28, $FF, $F8
+Map_ContinueIcon_Mighty1:
+		dc.w 2
+		dc.b  $F4,   5,   0, $2E, $FF, $F4
+		dc.b    4,   8,   0, $32, $FF, $F4
+Map_ContinueIcon_Mighty2:
+		dc.w 2
+		dc.b  $F4,   5,   0, $2E, $FF, $F4
+		dc.b    4,   8,   0, $35, $FF, $F4
+Map_ContinueIcon_Ray1:
+		dc.w 2
+		dc.b  $F4,   5,   0, $38, $FF, $F4
+		dc.b    4,   8,   0, $3C, $FF, $F4
+Map_ContinueIcon_Ray2:
+		dc.w 2
+		dc.b  $F4,   5,   0, $38, $FF, $F4
+		dc.b    4,   8,   0, $3F, $FF, $F4
+Map_ContinueIcon_MetalSonic1:
+		dc.w 2
+		dc.b  $F4,   5,   0, $42, $FF, $F7
+		dc.b    4,   4,   0, $46, $FF, $F7
+Map_ContinueIcon_MetalSonic2:
+		dc.w 2
+		dc.b  $F4,   5,   0, $48, $FF, $F7
+		dc.b    4,   4,   0, $46, $FF, $F7
