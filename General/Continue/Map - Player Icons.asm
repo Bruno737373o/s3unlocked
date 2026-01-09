1763c14b23d3474d49b8654690aec3e344dee37a
Liliam Bronze <liliambronze@gmail.com>
2026-01-09 12:22:37 +0000

    Optimize continue screen mappings

--- General/Continue/Map - Player Icons.asm
+++ General/Continue/Map - Player Icons.asm
@@ -3,8 +3,8 @@ Map_ContinueIcons_:
 		dc.w word_5CD20-Map_ContinueIcons_
 		dc.w word_5CD2E-Map_ContinueIcons_
 		dc.w word_5CD36-Map_ContinueIcons_
-		dc.w word_5CD56-Map_ContinueIcons_
-		dc.w word_5CD5E-Map_ContinueIcons_
+		dc.w Map_ContinueIcon_Knuckles1-Map_ContinueIcons_
+		dc.w Map_ContinueIcon_Knuckles2-Map_ContinueIcons_
 		dc.w Map_ContinueIcon_Amy1-Map_ContinueIcons_
 		dc.w Map_ContinueIcon_Amy2-Map_ContinueIcons_
 		dc.w Map_ContinueIcon_Mighty1-Map_ContinueIcons_
@@ -27,42 +27,48 @@ word_5CD2E:	dc.w 1
 word_5CD36:	dc.w 1
 		dc.b  $F4,   6,   0, $10, $FF, $F8
 word_5CD3E:	dc.w 1
-		dc.b  $FC,   5,   0, $4C, $FF, $F2
+		dc.b  $FC,   5,   0, $44, $FF, $F2
 word_5CD46:	dc.w 1
-		dc.b  $FC,   5,   0, $50, $FF, $F2
+		dc.b  $FC,   5,   0, $48, $FF, $F2
 word_5CD4E:	dc.w 1
-		dc.b  $FC,   5,   0, $54, $FF, $F2
-word_5CD56:	dc.w 1
-		dc.b  $F4,   6, $40, $16, $FF, $F8
-word_5CD5E:	dc.w 1
-		dc.b  $F4,   6, $40, $1C, $FF, $F8
+		dc.b  $FC,   5,   0, $4C, $FF, $F2
+Map_ContinueIcon_Knuckles1:
+		dc.w 2
+		dc.b  $F4,   5, $40, $18, $FF, $F8
+		dc.b    4,   4, $40, $16, $FF, $F8
+Map_ContinueIcon_Knuckles2:
+		dc.w 2
+		dc.b  $F4,   5, $40, $18, $FF, $F8
+		dc.b    4,   4, $40, $1C, $FF, $F8
 Map_ContinueIcon_Amy1:
-		dc.w 1
-		dc.b  $F4,   6,   0, $22, $FF, $F8
+		dc.w 2
+		dc.b  $F4,   5,   0, $1E, $FF, $F8
+		dc.b    4,   4,   0, $22, $FF, $F8
 Map_ContinueIcon_Amy2:
-		dc.w 1
-		dc.b  $F4,   6,   0, $28, $FF, $F8
+		dc.w 2
+		dc.b  $F4,   5,   0, $1E, $FF, $F9
+		dc.b    4,   4,   0, $24, $FF, $F8
 Map_ContinueIcon_Mighty1:
 		dc.w 2
-		dc.b  $F4,   5,   0, $2E, $FF, $F4
-		dc.b    4,   8,   0, $32, $FF, $F4
+		dc.b  $F4,   5,   0, $26, $FF, $F4
+		dc.b    4,   8,   0, $2A, $FF, $F4
 Map_ContinueIcon_Mighty2:
 		dc.w 2
-		dc.b  $F4,   5,   0, $2E, $FF, $F4
-		dc.b    4,   8,   0, $35, $FF, $F4
+		dc.b  $F4,   5,   0, $26, $FF, $F4
+		dc.b    4,   8,   0, $2D, $FF, $F4
 Map_ContinueIcon_Ray1:
 		dc.w 2
-		dc.b  $F4,   5,   0, $38, $FF, $F4
-		dc.b    4,   8,   0, $3C, $FF, $F4
+		dc.b  $F4,   5,   0, $30, $FF, $F4
+		dc.b    4,   8,   0, $34, $FF, $F4
 Map_ContinueIcon_Ray2:
 		dc.w 2
-		dc.b  $F4,   5,   0, $38, $FF, $F4
-		dc.b    4,   8,   0, $3F, $FF, $F4
+		dc.b  $F4,   5,   0, $30, $FF, $F4
+		dc.b    4,   8,   0, $37, $FF, $F4
 Map_ContinueIcon_MetalSonic1:
 		dc.w 2
-		dc.b  $F4,   5,   0, $42, $FF, $F7
-		dc.b    4,   4,   0, $46, $FF, $F7
+		dc.b  $F4,   5,   0, $3A, $FF, $F7
+		dc.b    4,   4,   0, $3E, $FF, $F7
 Map_ContinueIcon_MetalSonic2:
 		dc.w 2
-		dc.b  $F4,   5,   0, $48, $FF, $F7
-		dc.b    4,   4,   0, $46, $FF, $F7
+		dc.b  $F4,   5,   0, $40, $FF, $F7
+		dc.b    4,   4,   0, $3E, $FF, $F7
