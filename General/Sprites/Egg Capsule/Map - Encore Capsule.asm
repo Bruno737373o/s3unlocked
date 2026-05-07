2cb84e95eb889ea213d4992bff0cff967bf40570
Liliam Bronze <liliambronze@gmail.com>
2026-05-07 20:30:43 +0100

    Implement Encore capsule object

---
+++ General/Sprites/Egg Capsule/Map - Encore Capsule.asm
@@ -0,0 +1,35 @@
+Map_EncoreCapsule_:
+		dc.w Map_EncoreCapsule0-Map_EncoreCapsule_
+		dc.w Map_EncoreCapsule1-Map_EncoreCapsule_
+		dc.w Map_EncoreCapsule2-Map_EncoreCapsule_
+		dc.w Map_EncoreCapsule3-Map_EncoreCapsule_
+		dc.w Map_EncoreCapsule4-Map_EncoreCapsule_
+Map_EncoreCapsule0:
+		dc.w 7
+		dc.b  $E0,   8,   0,   0, $FF, $E8
+		dc.b  $E0,   8,   8,   0,   0,   0
+		dc.b  $E8,   7,   0,   3, $FF, $E0
+		dc.b  $E8,  $F,   8,  $B, $FF, $F0
+		dc.b  $E8,   7,   8,   3,   0, $10
+		dc.b    8,  $E,   0, $1B, $FF, $E0
+		dc.b    8,  $E,   8, $1B,   0,   0
+Map_EncoreCapsule1:
+		dc.w 3
+		dc.b  $F4,   5,   0, $27, $FF, $F0
+		dc.b  $F4,   5,   8, $27,   0,   0
+		dc.b  $14,  $A,   0, $2B, $FF, $F4
+Map_EncoreCapsule2:
+		dc.w 3
+		dc.b  $FB,   5,   0, $27, $FF, $F0
+		dc.b  $FB,   5,   8, $27,   0,   0
+		dc.b  $14,  $A,   0, $2B, $FF, $F4
+Map_EncoreCapsule3:
+		dc.w 3
+		dc.b  $EE,   6,   0, $EC, $FF, $F0
+		dc.b  $EE,   6,   0, $EC,   0,   0
+		dc.b  $EC,   6,   0, $FE, $FF, $F8
+Map_EncoreCapsule4:
+		dc.w 3
+		dc.b  $EC,   6,   0, $EC, $FF, $F0
+		dc.b  $EC,   6,   0, $EC,   0,   0
+		dc.b  $EE,   6,   0, $FE, $FF, $F8
